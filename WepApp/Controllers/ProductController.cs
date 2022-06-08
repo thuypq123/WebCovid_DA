e4d2f8329cdc41c431c232528b1aeada18700a3e
thuypq123 <lmthuy37@gmail.com>
2022-06-08 21:24:20 +0700

    THUY UPDATE

--- WepApp/Controllers/ProductController.cs
+++ WepApp/Controllers/ProductController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WepApp.Models;
 
 namespace WepApp.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly covid19Context _context;
+
+        public ProductController()
+        {
+            _context = new covid19Context();
+        }
+
         public IActionResult Index()
         {
+            var sanpham = _context.Sanphams.ToList();
+            return View(sanpham);
+        }
+        public IActionResult Details(int id)
+        {
+            //var sanpham = _context.Sanphams.Include(x => x.MadmNavigation).FirstOrDefault(x => x.Masp == id);
+            //if (sanpham == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
             return View();
         }
-        public IActionResult Details()
+        public IActionResult Pagecarts()
         {
             return View();
         }
