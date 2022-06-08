222f76dec2d07ac4df5e2f3677423e4bbaffa60d
thuypq123 <lmthuy37@gmail.com>
2022-06-08 17:49:01 +0700

    thuy

--- WepApp/Controllers/ProductController.cs
+++ WepApp/Controllers/ProductController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WepApp.Models;
 
 namespace WepApp.Controllers
 {
     public class ProductController : Controller
     {
+        covid19Context db = new covid19Context();
         public IActionResult Index()
         {
-            return View();
+            var sanpham = db.Sanphams.ToList();
+            return View(sanpham);
         }
         public IActionResult Details()
         {
